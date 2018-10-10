e99f29383efaadff64b60537c96a253653673490
snahian <contactsajednahian@gmail.com>
2018-10-10 17:28:55 -0400

    Fixed formatting

--- main.c
+++ main.c
@@ -3,30 +3,29 @@
 #include "linkedlist.h"
 
 int main() {
-
 	struct node * myNumbers;
 	printf("Printing empty linked list:\n");
 	print_list (myNumbers);
 
 	printf("-------------------------------\n");
 	printf("Inserting 1 to front: \n");
-	myNumbers = insert_front(0, 1);
+	myNumbers = insert_front(myNumbers, 1);
 	print_list (myNumbers);
 	
 	printf("-------------------------------\n");
 	printf("Inserting 2 to front: \n");
-	struct node * myNumbers1 = insert_front(myNumbers, 2);
-	print_list (myNumbers1);
+	myNumbers = insert_front(myNumbers, 2);
+	print_list (myNumbers);
 
 	printf("-------------------------------\n");
 	printf("Inserting 3 to front: \n");
-	struct node * myNumbers2 = insert_front(myNumbers1, 3);
-	print_list (myNumbers2);
+	myNumbers = insert_front(myNumbers, 3);
+	print_list (myNumbers);
 
 	printf("-------------------------------\n");
 	printf("Freeing list:\n");
-	struct node * myNumbers3 = free_list(myNumbers2);
-	print_list(myNumbers3);
+	myNumbers = free_list(myNumbers);
+	print_list(myNumbers);
 
 	return 0;
 }
